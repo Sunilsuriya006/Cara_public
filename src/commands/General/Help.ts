905fe8c0937d7e10c8b84832cdaa583492e99c03
SUNIL KUMAR <91380100+Sunilsuriya006@users.noreply.github.com>
2021-12-03 05:00:02 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
      run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
            const n = [
-           'https://i.pinimg.com/564x/80/c1/99/80c1992c17dc44e8731a6552f81e7c31.jpg'
+           'https://giphy.com/gifs/one-punch-man-saitama-genos-arbHBoiUWUgmc'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻 (❤ω❤) Konichiwa! *${M.sender.username}*\n\n•Support me by subscribing My youtube channel⬇️\nhttps://youtube.com/channel/UCIA_gt6ns4hu6k-WQOPRoNQ\n\n━━❰･Notes📮Side･❱━━\n*Read the rules*\n\nMy name is Cara\n\nMy prefix is !\n\n1 | *Don't Call* Bots to avoid Blocking.\n\n2 | *Don't Spam* in Groups&Pm to avoid Blocking\n\n`
+            let text = `👋🏻 (❤ω❤) Konichiwa! *${M.sender.username}*\n\n•Support me by subscribing My youtube channel⬇️\nhttps://youtube.com/channel/UCIA_gt6ns4hu6k-WQOPRoNQ\n\n━━❰･Notes📮Side･❱━━\n*Read the rules*\n\nMy name is saitama\n\nMy prefix is .\n\n1 | *Don't Call* Bots to avoid Blocking.\n\n2 | *Don't Spam* in Groups&Pm to avoid Blocking\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `━━❰•Bot ${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}•❱━━\n• \`\`\`${categories[
