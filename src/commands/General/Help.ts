bec16a99ee82206c8d12144fe5ea92fd22f32ddc
SUNIL KUMAR <91380100+Sunilsuriya006@users.noreply.github.com>
2021-12-05 14:03:55 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -35,10 +35,10 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻 Hello there! *${M.sender.username}*\n👊━❰･Notes📮Side･❱━👊\n*Read the rules*\n\n\nI'm saitama My prefix is *.* My support group link *https://chat.whatsapp.com/C8omneFl0I3Ha7PJRl9aDq*\n\n1 | *Don't Call* Bots to avoid Blocking.\n\n2 | *Don't Spam* in Groups&Pm to avoid Blocking\n\n`
+            let text = `👋🏻 Hello there! *${M.sender.username}*\n🌃 🌈━❰･Notes📮Side･❱━🌃 🌈\n*Read the rules*\n\n\nI'm saitama My prefix is *.* My support group link *https://chat.whatsapp.com/C8omneFl0I3Ha7PJRl9aDq*\n\n1 | *Don't Call* Bots to avoid Blocking.\n\n2 | *Don't Spam* in Groups&Pm to avoid Blocking\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
-                text += `👊Bot ${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}👊\n• \`\`\`${categories[
+                text += `🎆Bot ${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}🎆\n• \`\`\`${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
