d6bf2fa93988a6deaf6b84047a21f829ce15f228
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-05 17:09:13 +0100

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -1,25 +1,29 @@
-import MessageHandler from '../Handlers/MessageHandler'
-import {MessageType} from '@adiwajshing/baileys';
-import BaseCommand from '../lib/BaseCommand'
-import WAClient from '../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../typings'
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'support',
-            description: 'send you a Support link',
+            description: 'link you to support menu',
             category: 'general',
-            usage: `${client.config.prefix}support`
+            usage: `${client.config.prefix}support`,
+            aliases: ['support'],
+            baseXp: 20
         })
     }
 
-    
-    run = async (M: ISimplifiedMessage, args: IParsedArgs): Promise<void> => {
-
-const text = "your link goes here"
-
-M.reply('send you link regarding this');
-this.client.sendMessage(M.sender.jid,text,MessageType.text);
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await this.client.sendMessage(
+            M.sender.jid,
+            `*🎗️ 🅂🅄🄽🄸🄻-🄱🄾🅃🅃🄾*\n\n *📮【About】:* *Fun Bot*\n\n*🛸【Group】:* https://chat.whatsapp.com/EBUKlcbfc3692tllAcEAP7 \n`,
+            MessageType.text
+        )
+        return void M.reply('Sent you the support link in personal message'); //.catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
+    }
 }
-}
+
+
