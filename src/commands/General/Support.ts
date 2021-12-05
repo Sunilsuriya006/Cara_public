4eff0dd02513a14647810384a75fd0241368918b
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-05 16:59:16 +0100

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -1,41 +1,25 @@
 import MessageHandler from '../Handlers/MessageHandler'
-
 import {MessageType} from '@adiwajshing/baileys';
-
 import BaseCommand from '../lib/BaseCommand'
-
 import WAClient from '../lib/WAClient'
-
 import { IParsedArgs, ISimplifiedMessage } from '../typings'
 
 export default class Command extends BaseCommand {
-
     constructor(client: WAClient, handler: MessageHandler) {
-
         super(client, handler, {
-
             command: 'support',
-
             description: 'send you a Support link',
-
             category: 'general',
-
             usage: `${client.config.prefix}support`
-
         })
-
     }
 
     
-
     run = async (M: ISimplifiedMessage, args: IParsedArgs): Promise<void> => {
 
-const text = your link goes here
+const text = "your link goes here"
 
-M.reply('send you link regarding this);
-
+M.reply('send you link regarding this');
 this.client.sendMessage(M.sender.jid,text,MessageType.text);
-
 }
-
 }
